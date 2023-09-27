8cee3fab4e43130a09cbb013c9aa183bd5bca6c4
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2023-09-27 15:33:20 +0530

    mobile fixed

---
+++ scripts/headline.js
@@ -0,0 +1,18 @@
+const skills = [
+  "WEB DEVELOPER",
+  "GRAPHIC DESIGNER",
+  "VIDEO EDITOR",
+  "MOTION GRAPHICS DESIGNER",
+];
+
+const headlineElement = document.getElementById("headline");
+
+let currentIndex = 0;
+
+function updateSkill() {
+  headlineElement.innerHTML = skills[currentIndex];
+  currentIndex = (currentIndex + 1) % skills.length; // Cycle through skills in a loop
+  setTimeout(updateSkill, 3000); // Call this function again after 3 seconds
+}
+
+setTimeout(updateSkill, 300); // Call this function after 3 seconds
