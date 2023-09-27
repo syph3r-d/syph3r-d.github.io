09d1e22dd9e169385f1ed3cc2561fda83dc6859f
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2023-09-27 17:21:34 +0530

    project details

--- scripts/headline.js
+++ scripts/headline.js
@@ -1,20 +1,24 @@
 const skills = [
   "WEB DEVELOPER",
+  "BSC. ENGINEERING UNDERGRADUATE",
   "GRAPHIC DESIGNER",
-  "VIDEO EDITOR",
-  "MOTION GRAPHICS DESIGNER",
+  "MOTION EDITOR",
 ];
 
 const headlineElement = document.getElementById("headline");
 
-headlineElement.classList.add("rotation")
+const trinagle = document.getElementById("trianglesvg");
+
+trinagle.style.animation="polygon-morph 3s ease-in-out infinite"
+
+headlineElement.style.animation="polygon 3s ease-in-out infinite"
 
 let currentIndex = 0;
 
 function updateSkill() {
   headlineElement.innerHTML = skills[currentIndex];
-  currentIndex = (currentIndex + 1) % skills.length; // Cycle through skills in a loop
-  setTimeout(updateSkill, 3000); // Call this function again after 3 seconds
+  currentIndex = (currentIndex + 1) % skills.length; 
+  setTimeout(updateSkill, 3000); 
 }
 
-setTimeout(updateSkill, 300); // Call this function after 3 seconds
+setTimeout(updateSkill, 300);
