ed866cb6f8ee81e6c86b538b61ce41151a8307fa
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2023-09-27 23:32:39 +0530

    animation change

--- scripts/headline.js
+++ scripts/headline.js
@@ -1,24 +1,30 @@
 const skills = [
   "WEB DEVELOPER",
-  "BSC. ENGINEERING UNDERGRADUATE",
   "GRAPHIC DESIGNER",
   "MOTION EDITOR",
 ];
 
-const headlineElement = document.getElementById("headline");
+
+const headlines= document.querySelectorAll(".headline1");
 
 const trinagle = document.getElementById("trianglesvg");
 
 trinagle.style.animation="polygon-morph 3s ease-in-out infinite"
 
-headlineElement.style.animation="polygon 3s ease-in-out infinite"
+headlines.forEach((headlineElement,index) => {
+  headlineElement.style.animation="fade-left 3s ease-in-out infinite"
+  headlineElement.style.opacity="0.08"
+  headlineElement.style.animationDelay=`${index*0.01}s`
+});
 
 let currentIndex = 0;
 
 function updateSkill() {
-  headlineElement.innerHTML = skills[currentIndex];
+  headlines.forEach((headline) => {
+    headline.innerHTML = skills[currentIndex];
+  });
   currentIndex = (currentIndex + 1) % skills.length; 
   setTimeout(updateSkill, 3000); 
 }
 
-setTimeout(updateSkill, 300);
+setTimeout(updateSkill);
