017dd66bd33bd5c920c79f2f92dc45ad57ee19f4
Sadeepa Dhananjaya <sadeepaekanayaked@gmail.com>
2023-09-28 08:03:05 +0530

    animation fix

--- scripts/headline.js
+++ scripts/headline.js
@@ -1,30 +1,27 @@
-const skills = [
-  "WEB DEVELOPER",
-  "GRAPHIC DESIGNER",
-  "MOTION EDITOR",
-];
+const skills = ["WEB DEVELOPER", "GRAPHIC DESIGNER", "MOTION EDITOR"];
 
+const headlines = document.querySelectorAll(".headline1");
+const triangle = document.getElementById("trianglesvg");
 
-const headlines= document.querySelectorAll(".headline1");
-
-const trinagle = document.getElementById("trianglesvg");
-
-trinagle.style.animation="polygon-morph 3s ease-in-out infinite"
-
-headlines.forEach((headlineElement,index) => {
-  headlineElement.style.animation="fade-left 3s ease-in-out infinite"
-  headlineElement.style.opacity="0.08"
-  headlineElement.style.animationDelay=`${index*0.01}s`
-});
-
+// Set the animation for the triangle
+triangle.style.animation = "polygon-morph 3s ease-in-out infinite";
 let currentIndex = 0;
 
 function updateSkill() {
-  headlines.forEach((headline) => {
-    headline.innerHTML = skills[currentIndex];
+  headlines.forEach((headlineElement, index) => {
+    headlineElement.style.opacity = "0";
+    headlineElement.style.animation = "none"; // Clear the animation
+    setTimeout(() => {
+      headlineElement.innerHTML = skills[currentIndex];
+      headlineElement.style.animation = "fade-left 3s ease-in-out"; // Apply animation
+      headlineElement.style.animationDelay = `${index * 0.01}s`;
+    }); // Small delay to allow clearing animation
   });
-  currentIndex = (currentIndex + 1) % skills.length; 
-  setTimeout(updateSkill, 3000); 
+
+  currentIndex = (currentIndex + 1) % skills.length;
+
+  setTimeout(updateSkill, 3000);
 }
 
 setTimeout(updateSkill);
+
